04d94f9c012ee9040234defd9c7a1af5557ba79b
Oleksandr <aleks6699@gmail.com>
2024-09-30 17:23:35 +0300

    feat: add function create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { join } from "path";
+import { existsSync, writeFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const create = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, "files", "fresh.txt");
+  const content = "I am fresh and young";
+
+  try {
+    if (existsSync(filePath)) {
+      throw new Error("FS operation failed");
+    }
+
+    writeFileSync(filePath, content, { flag: "wx" });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await create();
