cc6b9f90feed011f0a83b381f49f3bf781db9277
Oleksandr <aleks6699@gmail.com>
2024-10-02 19:45:25 +0300

    feat: add function copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+  const filesDir = path.join(__dirname, "files");
+  const filesCopyDir = path.join(__dirname, "files_copy");
+
+  try {
+    if (!fs.existsSync(filesDir)) {
+      throw new Error("FS operation failed: files directory does not exist");
+    }
+
+    if (fs.existsSync(filesCopyDir)) {
+      throw new Error(
+        "FS operation failed: files_copy directory already exists"
+      );
+    }
+
+    await fs.promises.cp(filesDir, filesCopyDir, { recursive: true });
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
